1382c4373ad6f25e01e18b4d1db2c63a4268b9bd
FELARDOS Mathis <felard_m@epita.fr>
2015-07-11 01:45:40 +0200

    Project: SFML now works

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,35 @@
 // Created by veyry_p on 7/9/15.
 //
 
+#include "features/integral-image.h"
+/*
 int main(void)
 {
   return 0;
+}
+ */
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
