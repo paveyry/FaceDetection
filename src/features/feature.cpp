b3b85fab1bf3a41c6daf8b263a297027631b83b0
FELARDOS Mathis <felard_m@epita.fr>
2015-07-12 00:44:11 +0200

    Features: abstract class feature

---
+++ src/features/feature.cpp
@@ -0,0 +1,16 @@
+//
+// Created by hasb4k on 7/11/15.
+//
+
+#include "feature.h"
+
+namespace violajones
+{
+
+  int Feature::normalize_feature(int value, int derivation)
+  {
+    /* 40 is the average value of the derivation */
+    return (value * 40) / derivation;
+  }
+
+}
---
+++ src/features/feature.h
@@ -0,0 +1,32 @@
+//
+// Created by hasb4k on 7/11/15.
+//
+
+#ifndef FACERECOG_FEATURES_H
+# define FACERECOG_FEATURES_H
+
+#include "../tools/point.h"
+#include "../image/integral-image.h"
+#include "../tools/rectangle.h"
+
+namespace violajones
+{
+  class Feature
+  {
+  public:
+    virtual ~Feature() = default;
+
+    virtual int compute_value(Point win_top_left, float size_ratio, IntegralImage image) = 0;
+
+    virtual int compute_value(IntegralImage image) = 0;
+
+    static int normalize_feature(int value, int derivation);
+
+  public:
+    Rectangle frame;
+
+  };
+
+}
+
+#endif /* !FACERECOG_FEATURES_H */
