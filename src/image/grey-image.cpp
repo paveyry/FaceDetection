35ac343b8189970cd2b48ac389dd3ff82d483f18
FELARDOS Mathis <felard_m@epita.fr>
2015-07-11 05:16:28 +0200

    Images: Grey Images

---
+++ src/image/grey-image.cpp
@@ -0,0 +1,35 @@
+//
+// Created by hasb4k on 7/11/15.
+//
+
+#include "grey-image.h"
+
+namespace violajones
+{
+  GreyImage::GreyImage(sf::Image image)
+  {
+    pixels = std::make_shared<sf::Image>(image);
+  }
+
+  GreyImage::GreyImage(const std::string path)
+  {
+    pixels = std::make_shared<sf::Image>();
+    pixels->loadFromFile(path);
+    create_pixels();
+  }
+
+  void GreyImage::create_pixels()
+  {
+    auto size = pixels->getSize();
+    width = size.x;
+    height = size.y;
+
+    for (int x = 0; x < width; ++x)
+      for (int y = 0; y < width; ++y)
+      {
+        auto color = pixels->getPixel(x, y);
+        sf::Uint8 grey = (sf::Uint8) (((int) color.r + color.g + color.b) / 3);
+        pixels->setPixel(x, y, sf::Color{grey, grey, grey});
+      }
+  }
+}
---
+++ src/image/grey-image.h
@@ -0,0 +1,36 @@
+//
+// Created by hasb4k on 7/11/15.
+//
+
+#ifndef FACE_RECOG_GREY_IMAGE_H
+# define FACE_RECOG_GREY_IMAGE_H
+#include <bits/stringfwd.h>
+#include <SFML/Graphics/Image.hpp>
+#include <memory>
+
+typedef unsigned char byte;
+typedef char sbyte;
+
+namespace violajones
+{
+  class GreyImage
+  {
+  public:
+    /* Constructors */
+    GreyImage(sf::Image image);
+    GreyImage(std::string path);
+
+  protected:
+    void create_pixels();
+
+
+  public:
+
+    std::shared_ptr<sf::Image> pixels;
+    int width;
+    int height;
+  };
+
+}
+
+#endif /* FACE_RECOG_GREY_IMAGE_H */
