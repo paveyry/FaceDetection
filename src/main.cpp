355b3aeb5dbb223d8ef99a7a3ddd5c3f2abe8ab3
Pierre-Alexandre VEYRY <veyry_p@epita.fr>
2015-07-13 23:36:49 +0200

    Added optparser

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "trainer/strong-classifier.h"
 
 using namespace violajones;
+namespace po = boost::program_options;
 
 void print_usage(void)
 {
@@ -19,52 +20,12 @@ void print_usage(void)
   << std::endl;
 }
 
-int main(int argc, char** argv)
+static void load(po::variables_map& vm)
 {
-  /*
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
-
-  while (window.isOpen())
-  {
-      sf::Event event;
-      while (window.pollEvent(event))
-      {
-          if (event.type == sf::Event::Closed)
-              window.close();
-      }
-
-      window.clear();
-      window.draw(shape);
-      window.display();
-  }
-  return 0;
-  */
-  /*
-  std::string imagepath;
-  std::string traindirpath;
-  std::string method;
-  std::string output_classifier;
-  namespace po = boost::program_options;
-  po::options_description desc("Options");
-  desc.add_options()
-          ("help,h", "Print help messages")
-          ("method,m", po::value<std::string>(&method)->required(),"Select the method (load or train")
-          ("image,i", po::value<std::string>(&imagepath)->required(), "Specify the input image")
-          ("dir, d", po::value<std::string>(&traindirpath)->required(), "Specify the training dir")
-          ("outclassif,o", po::value<std::string>(&output_classifier), "Specify the output classifier file");
-
-  try
-  {
-    po::store(po::command_line_parser(argc, argv).options(desc))
-  }*/
-  //StrongClassifier classifier;
-
-  auto classifier = StrongClassifier::load_from_file("/home/hasb4k/classifier");
-  Detector detector{"/home/hasb4k/input.png", classifier};
+  auto classifier = StrongClassifier::load_from_file(vm["file"].as<std::string>());
+  Detector detector{vm["image"].as<std::string>(), classifier};
   auto d = detector.detect();
-  std::cout << d.size()<<std::endl;
+  std::cout << d.size() << std::endl;
 
   int i = 0;
   for (auto& rect : d)
@@ -72,6 +33,72 @@ int main(int argc, char** argv)
     std::cerr << "Curr i: " << i++ << " " << rect.to_string() << std::endl;
     rect.draw(detector.image_->image.pixels);
   }
-  detector.image_->image.pixels->saveToFile("/home/hasb4k/output.png");
+  std::shared_ptr<sf::Image> img = detector.image_->image.pixels;
+  detector.image_->image.pixels->saveToFile(vm["saveimage"].as<std::string>());
+  sf::RenderWindow window(sf::VideoMode(img->getSize().x, img->getSize().y), "Output image");
+  sf::Texture texture;
+  texture.loadFromImage(*img);
+  sf::Sprite sprite(texture);
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::KeyPressed)
+        window.close();
+      window.clear();
+      window.draw(sprite);
+      window.display();
+    }
+  }
+}
+
+static void train(po::variables_map& vm)
+{
+
+}
+
+int main(int argc, char** argv)
+{
+  po::options_description desc("Options");
+  desc.add_options()
+          ("help,h", "Print help messages")
+          ("method,m", po::value<std::string>(), "Select the method (load or train")
+          ("image,i", po::value<std::string>(), "Specify the input image")
+          ("dir,d", po::value<std::string>(), "Specify the training dir")
+          ("saveimage,s", po::value<std::string>(), "Specify the output image name")
+          ("classif,c", po::value<std::string>(), "Specify the classifier to use")
+          ("outclassif,o", po::value<std::string>(), "Specify the output classifier file");
+
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
+  po::notify(vm);
+  if (vm.count("help"))
+    std::cout << desc << std::endl;
+  else if (vm.count("method"))
+  {
+    if (vm["method"].as<std::string>() == "load")
+    {
+      if (!vm.count("file"))
+        std::cout << "Please specify an input classifier" << std::endl;
+      if (!vm.count("image"))
+        std::cout << "Please specify an input image" << std::endl;
+      if (!vm.count("saveimage"))
+        std::cout << "Please specify the path to save the image" << std::endl;
+      load(vm);
+    }
+    else if (vm["method"].as<std::string>() == "train")
+    {
+      train(vm);
+    }
+    else
+      std::cout << "Please specify an existing method" << std::endl;
+  }
+  else
+    std::cout << "Please specify a method" << std::endl;
+
+
+
+
   return 0;
 }
