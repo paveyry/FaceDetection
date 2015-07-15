fb9a3fcfc9809272ddf9cdcb448379ad600ec6da
FELARDOS Mathis <felard_m@epita.fr>
2015-07-15 06:47:30 +0200

    detector: add parallized foreach

--- src/detector/detector.cpp
+++ src/detector/detector.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <iostream>
+#include <tbb/tbb.h>
+#include <chrono>
 #include "../config.h"
 #include "detector.h"
 
@@ -10,17 +12,31 @@ namespace violajones
 {
   std::vector<Rectangle> Detector::detect()
   {
-    std::function<bool(Window)> check = [&](Window win){ return classifier_.check(win, image_); };
+
     std::vector<Rectangle> rectvect;
     auto wins = Window::list_windows(image_, squared_image_);
     if (Config::debug_detector_detect)
       std::cout << "Windowlist :" << wins.size() << std::endl;
 
-    for (Window& w : wins)
-    {
-      if (check(w))
-        rectvect.push_back(w.to_rectangle());
-    }
+
+    static tbb::mutex mutex;
+    if (Config::parallelized)
+      tbb::parallel_for_each(wins.begin(), wins.end(), [&](Window &w) {
+        if (classifier_.check(w, image_))
+        {
+          auto rect = w.to_rectangle();
+          tbb::mutex::scoped_lock lock;
+          lock.acquire(mutex);
+          rectvect.push_back(rect);
+          lock.release();
+        }
+      });
+    else
+      for (Window &w : wins)
+        if (classifier_.check(w, image_))
+          rectvect.push_back(w.to_rectangle());
+
+
     return rectvect;
   }
 }
